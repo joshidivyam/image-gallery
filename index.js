5a6a47b0e7e75fa2c824fb76a1167a92949a411a
joshidivyam <joshidivyam67@gmail.com>
2026-05-07 23:34:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// document.querySelector('form').addEventListener('submit', function (e) {
+
+//     const fname = document.getElementsByClassName('fname').value.trim();
+//     const lname = document.getElementByClassName('lname').value.trim();
+//     const email = document.getElementByClassName('email').value.trim();
+//     const phno = document.getElementByClassName('phnos').value.trim();
+
+
+//     if (fname === "" || lname === "" || email === "" || phnos === "") {
+//         e.preventDefault();
+//         alert("Please fill all fields!");
+//         return;
+//     }
+
+    
+//     if (!email.includes("@") || !email.includes(".")) {
+//         e.preventDefault();
+//         alert("Please enter a valid email address!");
+//         return;
+//     }
+
+  
+//     if (phnos.length !== 10 || isNaN(phnos)) {
+//         e.preventDefault();
+//         alert("Contact number must be exactly 10 digits!");
+//         return;
+//     }
+
+
+// });
